7296fc2e306f28a6b068a650b683476455b728e3
Thomas Asadi Espaas <46780228+Thomas-Espaas@users.noreply.github.com>
2023-02-21 06:48:53 +0100

    Syntax

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import UncertainModels
 if __name__ == '__main__':
     print('Running')
 
-    ## To look at a single problem and uncertainty model
+    # To look at a single problem and uncertainty model
 
     uncertain_pooling_object = UncertainModels.ScenarioPooling('Haverly_2', std=0.02, scen_gen_strat='Lee')
 
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     uncertain_pooling_object.save_results()
 
-    ## To run several problems, standard deviations and models one after another
+    # To run several problems, standard deviations and models one after another
 
     """for problem in ['Haverly_1', 'Haverly_2', 'Haverly_3', 'Foulds_2']:
         for standard_deviation in [0.2, 0.02, 0.002, 0.0002]:
